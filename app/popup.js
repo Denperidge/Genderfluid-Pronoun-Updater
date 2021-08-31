bd3ea17f8e661dda9baf5411f525b6d6e56410e1
Denperidge <denperidge@gmail.com>
2021-08-31 19:22:08 +0200

    WIP Facebook pronoun setting

---
+++ app/popup.js
@@ -0,0 +1,11 @@
+// Facebook url: https://www.facebook.com/profile.php?sk=about_contact_and_basic_info
+
+function SetPronouns(e) {
+    console.log(e);
+    console.log(this);
+
+    SetOnFacebook();
+
+}
+
+function SetOnFacebook() {}
