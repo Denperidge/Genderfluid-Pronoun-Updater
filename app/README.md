69bf4e67d2bc0d607b6c01baf7d3a68c4e8dc913
Denperidge <denperidge@gmail.com>
2021-09-01 17:19:51 +0200

    First draft of dev popup

---
+++ app/README.md
@@ -0,0 +1,9 @@
+
+
+## Gender codes
+- m == he/him
+- f == she/her
+- n == they/them
+
+**Context behind the gender codes:**
+Some sites (like Facebook) do not have a textbox or a status field to put pronouns in; instead opting for a select box containing pronouns to choose from. In Facebooks case - and this is the case for most websites - the selection is limited to male/masculine (he/him), female/feminine (she/her) or gender neutral (they/them). However, the naming schemes aren't consistent. Everything from "X" to "Rather not say" to "non-binary" or "custom gender". To make things non-confusing across development, there'll be 3 set codes (1 lowercase character) for which of those options should be selected. These are m for masc, f for femme, and n for gender neutral.
--- app/popup.js
+++ app/popup.js
@@ -1,11 +1,32 @@
 // Facebook url: https://www.facebook.com/profile.php?sk=about_contact_and_basic_info
 
 function SetPronouns(e) {
-    console.log(e);
-    console.log(this);
-
-    SetOnFacebook();
+    var mainPronoun = this.value[0].toLowerCase();
+    SetOnFacebook(mainPronoun);
 
 }
 
-function SetOnFacebook() {}
+function SetOnFacebook(mainPronoun) {
+    console.log(mainPronoun)
+    chrome.tabs.create({
+        url: "https://www.facebook.com/profile.php?sk=about_contact_and_basic_info"
+    }).then((tab) => {
+        var tabId = tab.id;
+        
+        console.log(tab);
+    }, (err) => {
+        console.error(err);
+    });
+    
+}
+
+
+
+$("button").click(SetPronouns);
+
+/*
+var pronounButtons = document.getElementsByClassName("pronouns");
+for (var i=0; i<pronounButtons.length; i++) {
+    pronounButtons[i].addEventListener(SetPronouns)
+}
+*/
