eb8f18f0bb68fd048678a3f7b19cbe1308144c84
Denperidge <denperidge@gmail.com>
2021-09-02 20:05:42 +0200

    First succesful pronoun change on FB!

--- app/integrations/facebook.js
+++ app/integrations/facebook.js
@@ -1,47 +1,80 @@
-//function FacebookIntegration(gendercode) {
+function FacebookIntegration(gendercode) {
 
 
     // These are the selectors to be used to find the controls on Facebook
     var editGenderButton = {
-        selector: ".oajrlxb2.bp9cbjyn.tdjehn4e.mtkw9kbi.tlpljxtp.qensuy8j.ppp5ayq2.goun2846.ccm00jje.s44p3ltw.mk2mc5f4.rt8b4zig.n8ej3o3l.agehan2d.sk4xxmp2.rq0escxv.nhd2j8a9.j83agx80.mg4g778l.btwxx1t3.pfnyh3mw.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.tgvbjcpo.hpfvmrgz.jktsbyx5.d1544ag0.osnr6wyh.tw6a2znq.l9j0dhe7.hzawbc8m.esuyzwwr.f1sip0of.du4w35lb.lzcic4wl.abiwlrkh.p8dawk7l.dwo3fsh8.ow4ym5g4.auili1gw.beltcj47.p86d2i9g.aot14ch1.kzx2olss.i1fnvgqd",
-        index: 3,
+        selector: "div > div > div:nth-child(1) > div > div > div > div > div > div > div > div > div > div > div > div > div:nth-child(1) > div > div > div > div > div > div > div > div > div:nth-child(3) > div:nth-child(2) > div > div > div:nth-child(2) > div",
         domObject: null,
     }
 
     var editGenderSelectbox = {
-        selector: ".oajrlxb2.bp9cbjyn.tdjehn4e.mtkw9kbi.tlpljxtp.qensuy8j.ppp5ayq2.goun2846.ccm00jje.s44p3ltw.mk2mc5f4.rt8b4zig.n8ej3o3l.agehan2d.sk4xxmp2.rq0escxv.nhd2j8a9.j83agx80.mg4g778l.btwxx1t3.pfnyh3mw.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.tgvbjcpo.hpfvmrgz.jktsbyx5.d1544ag0.osnr6wyh.tw6a2znq.l9j0dhe7.hzawbc8m.esuyzwwr.f1sip0of.du4w35lb.lzcic4wl.abiwlrkh.p8dawk7l.dwo3fsh8.ow4ym5g4.auili1gw.beltcj47.p86d2i9g.aot14ch1.kzx2olss.i1fnvgqd",
-        index: 1,
+        selector: "div > div > div:nth-child(1) > div > div > div > div > div > div > div > div > div > div > div > div > div:nth-child(1) > div > div > div > div > div > div > div > div > div:nth-child(3) > div:nth-child(2) > div > div:nth-child(4) > div:nth-child(2) > div",
         domObject: null
     }
 
+    // https://stackoverflow.com/a/8714421
+
     var mOption = {
-        selector: "#jsc_c_1k__1",
+        selector: "div[id^=jsc][id$='1']",
         domObject: null
     }
 
     var fOption = {
-        selector: "#jsc_c_1k__0",
+        selector: "div[id^=jsc][id$='0']",
         domObject: null
     }
 
     var nOption = {
-        selector: "#jsc_c_1k__2",
+        selector: "div[id^=jsc][id$='2']",
         domObject: null
     }
 
-    editGenderButton.domObject = document.querySelectorAll(editGenderButtonSelector)[editGenderButton.index];
-    editGenderButton.domObject.click();
+    var saveButtonSelector = "div > div > div:nth-child(1) > div > div > div > div > div > div > div > div > div > div > div > div > div:nth-child(1) > div > div > div > div > div > div > div > div > div:nth-child(3) > div:nth-child(2) > div > div > div > div:nth-child(2)";
 
-    editGenderSelectbox = document.querySelectorAll(editGenderSelectbox.selector)[editGenderSelectbox.index];
-    editGenderSelectbox.domObject.click();
+    console.log(gendercode)
+
+    console.log("Clicking edit...")
+
+    console.log(editGenderButton.selector)
+
+    setTimeout(()=>{
+
+        //editGenderButton.domObject = document.querySelector(editGenderButton.selector);
+        //editGenderButton.domObject.click();
+        document.querySelector(editGenderButton.selector).click();
+
+        setTimeout(()=> {
+
+            console.log("Clicking select...")
+
+            //editGenderSelectbox.domObject = document.querySelector(editGenderSelectbox.selector);
+            console.log("Clicking edit...")
+            //editGenderSelectbox.domObject.click();
+            document.querySelector(editGenderSelectbox.selector).click();
+
+            console.log("Clicking option...")
+
+            setTimeout(() => {
+
+                var option;
+                if (gendercode == "m") option = mOption;
+                else if (gendercode == "f") option = fOption;
+                else option = nOption;
+        
+                //option.domObject = document.querySelector(option.selector);
+                //option.domObject.click();
+                console.log(option.selector)
+                document.querySelector(option.selector).click();
+
+                setTimeout(() => {
+                    document.querySelector(saveButtonSelector).click();
+                }, 500)
+            });
+            
+        }, 500)
+
+    }, 500)
 
 
-    var option;
-    if (gendercode == "m") option = mOption;
-    else if (gendercode == "f") option = fOption;
-    else option = nOption;
 
-    option.domObject = document.querySelector(option.selector);
-    option.domObject.click();
-
-//}
+}
--- app/popup.js
+++ app/popup.js
@@ -1,7 +1,8 @@
 // Facebook url: https://www.facebook.com/profile.php?sk=about_contact_and_basic_info
 
+var gendercode;
 function SetPronouns(e) {
-    var gendercode = this.value[0].toLowerCase();
+    gendercode = this.value[0].toLowerCase();
     /*chrome.permissions.request({
         permissions: ["scripting"],
         origins: ["https://www.facebook.com/"]});*/
@@ -9,6 +10,7 @@ function SetPronouns(e) {
 
 }
 
+var fbTabId;
 function SetOnFacebook(gendercode) {
     chrome.permissions.request({
         origins: ["https://www.facebook.com/profile.php"]
@@ -25,25 +27,9 @@ function SetOnFacebook(gendercode) {
         chrome.tabs.create({
             url: "https://www.facebook.com/profile.php?sk=about_contact_and_basic_info"
         }).then((tab) => {
-            let tabId = tab.id;
-    
-            /*
-            chrome.scripting.executeScript({
-                target: {tabId: tabId},
-                func: () => {
-                    var gendercode = "n";
-                }
-            });
-            */
-    
-    
-            chrome.scripting.executeScript({
-                target: {tabId: tabId},
-                files: ["/app/integrations/facebook.js"]//,
-                //args: [ gendercode ]
-            });
+            fbTabId = tab.id;
+
             
-            console.log(tab);
         }, (err) => {
             console.error(err);
         });    
@@ -51,6 +37,51 @@ function SetOnFacebook(gendercode) {
 
    
 }
+
+chrome.tabs.onActivated.addListener((activationInfo) => {
+    
+    if (activationInfo.tabId != fbTabId) {
+        return;
+    }
+    console.log("Activate: " + activationInfo.tabId)
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (tabId != fbTabId) {
+        console.log("Updated, but not facebook")
+        return;
+    }
+
+    console.log(changeInfo.status)
+
+    if (changeInfo.status != "complete") {
+        console.log("Updated, but not complete: " + changeInfo.status)
+        return;
+    }
+
+    console.log("Facebook complete!")
+
+    console.log(gendercode)
+
+
+    
+    chrome.scripting.executeScript({
+        target: {tabId: tabId},
+        func: FacebookIntegration,
+        args: [gendercode]
+        
+    });
+
+
+    return;
+    chrome.scripting.executeScript({
+        target: {"tabId": tabId},
+        files: ["/app/integrations/facebook.js"]//,
+        //args: [ gendercode ]
+    });
+    
+    console.log(tab);
+});
 
 
 
